901f178036cbe2e26182f02c69f59e537c9eddba
Gérard Collin <gerard.collin@allianz.com>
2020-04-09 10:53:16 +0200

    fixed e2e tests

--- apps/ide-ui-e2e/src/integration/app.spec.ts
+++ apps/ide-ui-e2e/src/integration/app.spec.ts
@@ -1,13 +1,16 @@
-import { getGreeting } from '../support/app.po';
+import { getMainEditor, getSidePanel, getToolbar, getToolbarTitle } from '../support/app.po';
 
 describe('ide-ui', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display correct layout', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+//    cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to ide-ui!');
+    getToolbar();
+    getToolbarTitle().contains('Non-Development Environment');
+    getSidePanel();
+    getMainEditor();
   });
 });
--- apps/ide-ui-e2e/src/support/app.po.ts
+++ apps/ide-ui-e2e/src/support/app.po.ts
@@ -1,1 +1,6 @@
-export const getGreeting = () => cy.get('h1');
+export const getSidePanel = () => cy.get('#mainSidePanel');
+export const getToolbar = () => cy.get('#mainToolbar');
+export const getToolbarTitle = () => getToolbar().within(() => {
+  return cy.get("span");
+});
+export const getMainEditor = () => cy.get('ide-ui-main-editor');
