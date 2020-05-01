591ef5daecf2325d9ef8a922fa63aceebabfe92c
Gérard Collin <gerard.collin@allianz.com>
2020-05-01 17:59:57 +0200

    Menu and Title auto updates

--- apps/ide-ui/src/app/shared/change/services/change-update.service.ts
+++ apps/ide-ui/src/app/shared/change/services/change-update.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Change } from '../change';
 import { BroadcastChannel } from 'broadcast-channel';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,27 @@ export class ChangeUpdateService {
   protected listOfChanges: Change[]=[];
   protected channel: BroadcastChannel<Change>;
 
+  myWebSocket: WebSocketSubject<Change>;
+
   constructor() {
     console.log('Creating channel');
     this.channel = new BroadcastChannel(ChangeUpdateService.CHANNEL_CHANGE_NAME);
+    this.myWebSocket = webSocket('ws://localhost:8080/ide');
+    this.myWebSocket.subscribe(
+      msg => console.log('message received: ' + msg),
+      // Called whenever there is a message from the server
+      err => console.log(err),
+      // Called if WebSocket API signals some kind of error
+      () => console.log('complete')
+      // Called when connection is closed (for whatever reason)
+    );
   }
 
   pushChange (newChange:Change) {
     console.log('Change pushed');
     this.listOfChanges.push(newChange);
     this.channel.postMessage(newChange);
+    this.myWebSocket.next(newChange);
   }
 
   getListOfChanges (): Change[] {
