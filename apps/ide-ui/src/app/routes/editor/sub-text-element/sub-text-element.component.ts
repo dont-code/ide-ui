edc638c1fde353c2da94aa8ee299a68b61a7b46a
Gérard Collin <gerard.collin@allianz.com>
2021-01-05 11:01:20 +0100

    Simplified EditorElement

--- apps/ide-ui/src/app/routes/editor/sub-text-element/sub-text-element.component.ts
+++ apps/ide-ui/src/app/routes/editor/sub-text-element/sub-text-element.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
-import { EditorElement } from '../editor-element';
-import { TextService } from '../../../shared/text/services/text.service';
+import { EditorElement } from "../editor-element";
+import { ChangeUpdateService } from "../../../shared/change/services/change-update.service";
+import { Change, ChangeType } from "@dontcode/core";
 
 @Component({
   selector: 'ide-ui-sub-text-element',
@@ -12,24 +13,54 @@ export class SubTextElementComponent implements OnInit, OnChanges {
   element: EditorElement;
 
   items: EditorElement[];
-  constructor(protected service:TextService) { }
+  constructor(protected changeService:ChangeUpdateService) { }
 
   ngOnInit(): void {
-//    this.items = this.element.getChildrenToDisplay();
   }
 
   addElement() {
     this.element.addSubElement();
   }
+
   removeElement(item:EditorElement, index:number) {
-    this.element.removeElement( item, index);
+    let removed = this.element.removeElement( item, index);
+    this.changeService.pushChange(
+      new Change(ChangeType.DELETE,
+        removed.position, null));
   }
 
+  /**
+   * Moves the item up one place and returns the immediately following item, if there is one.
+   * or returns null if nothing have been moved
+   * @param item
+   * @param index
+   */
   upElement(item:EditorElement, index:number) {
-    this.element.upElement( item, index);
+    let oldPos = item.position; // The old position is the same as the new one in this case (only the beforeId changes)
+    let nextElement = this.element.upElement( item, index);
+    if( nextElement !== null) {
+      let nextKey= nextElement.getItemIdIfExists();
+
+      this.changeService.pushChange(
+        new Change(ChangeType.MOVE, item.position, item.getEditedValue(),null, nextKey, oldPos)
+      );
+    }
   }
   downElement(item:EditorElement, index:number) {
-    this.element.downElement( item, index);
+    let oldPos = item.position; // The old position is the same as the new one in this case (only the beforeId changes)
+    let nextElement = this.element.downElement( item, index);
+    if( nextElement!==null) {
+      let nextKey;
+      if( nextElement===undefined) {  // It is the last element
+        nextKey = null;
+      } else {
+        nextKey=nextElement.getItemIdIfExists();
+      }
+      this.changeService.pushChange(
+          new Change(ChangeType.MOVE, item.position, item.getEditedValue(),null, nextKey, oldPos)
+        );
+
+    }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
