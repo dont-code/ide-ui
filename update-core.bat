8e284bd4f5c43706bd546512f1ec0ae3fc0f8749
gerard collin <gerard.collin@allianz.com>
2021-03-15 14:08:29 +0100

    Added fields plugin import

--- update-core.bat
+++ update-core.bat
@@ -1,7 +1,5 @@
 cd ..\core\node
 call nx run core:build
-call npm pack dist/libs/core
 cd ..\..\ide-ui
-del dontcode-core-*.tgz
-move ..\core\node\dontcode-core-*.tgz .
-forfiles /M dontcode-core-*.tgz /C "cmd /c npm install @file"
+call npm uninstall @dontcode/core
+call npm install "..\core\node\dist\libs\core"
--- update-plugins.bat
+++ update-plugins.bat
@@ -1,10 +1,6 @@
 cd ..\plugins
 call nx run screen:build --prod
-call npm pack dist/libs/screen
 call nx run fields:build --prod
-call npm pack dist/libs/fields
 cd ..\ide-ui
-del dontcode-plugin-*.tgz
-move ..\plugins\dontcode-plugin-screen-*.tgz .
-move ..\plugins\dontcode-plugin-fields-*.tgz .
-forfiles /M dontcode-plugin-*.tgz /C "cmd /c npm install @file"
+call npm uninstall @dontcode/plugin-screen @dontcode/plugin-fields
+call npm install "..\plugins\dist\libs\screen" "..\plugins\dist\libs\fields"
