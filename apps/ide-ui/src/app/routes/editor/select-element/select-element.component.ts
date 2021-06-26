f6a4ed9235ef2f4a140a1135a4965326df487af3
Gérard Collin <gerard.collin@appliweb.net>
2021-06-26 16:02:16 +0200

    Support for subValues in Enum

--- apps/ide-ui/src/app/routes/editor/select-element/select-element.component.ts
+++ apps/ide-ui/src/app/routes/editor/select-element/select-element.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 import {EditorElement} from '../editor-element';
 import {ChangeUpdateService} from '../../../shared/change/services/change-update.service';
-import {Change, ChangeType} from '@dontcode/core';
+import {Change, ChangeType, DontCodeSchemaEnumValue} from '@dontcode/core';
 
 @Component({
   selector: 'ide-ui-select-element',
@@ -15,6 +15,8 @@ export class SelectElementComponent implements OnInit {
 
   @Output()
   refresh = new EventEmitter<string>();
+
+  filteredElements:any[];
 
   constructor(protected changeService:ChangeUpdateService) {
   }
@@ -38,5 +40,38 @@ export class SelectElementComponent implements OnInit {
     if(changed) {
       this.refresh.emit('refresh');
     }
+  }
+
+  filterGroupedElements(event) {
+    const filteredGroups = [];
+    const defaultGroup = {
+      label: 'Standard',
+      items: null
+    }
+
+    event.query = event.query?.toLowerCase();
+
+    for (const optgroup of this.element.values as Array<DontCodeSchemaEnumValue>) {
+      if( !(optgroup.getChildren()) || (optgroup.getChildren().length===0)) {
+        if( optgroup.getLabel().toLowerCase().indexOf(event.query) >= 0) {
+          if (defaultGroup.items === null) {
+            defaultGroup.items = new Array();
+            filteredGroups.push(defaultGroup);
+          }
+          defaultGroup.items.push (optgroup.getLabel());
+        }
+      } else {
+        const items = optgroup.getChildren().filter (value => value.getLabel().toLowerCase().indexOf(event.query) >=0).map(value => value.getLabel());
+        if( items.length>0) {
+          filteredGroups.push({
+            label: optgroup.getLabel(),
+            value: optgroup.getValue(),
+            items: items
+          });
+        }
+      }
+    }
+
+    this.filteredElements = filteredGroups;
   }
 }
