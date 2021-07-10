a22774f8449d35e3f2b70dd5aca8a6b6e2d21384
Gérard Collin <gerard.collin@appliweb.net>
2021-07-10 10:19:24 +0200

    added currency library

--- update-links.sh
+++ update-links.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 cd ../core/node
 nx run core:build
-cd ../../plugins
+cd ../../ide-ui
+npm link ../core/node/dist/libs/core
+cd ../plugins
 nx run common:build --prod
 nx run basic:build --prod
 nx run screen:build --prod
 nx run fields:build --prod
 
 cd ../ide-ui
-npm link ../core/node/dist/libs/core ../plugins/dist/libs/common ../plugins/dist/libs/basic ../plugins/dist/libs/screen ../plugins/dist/libs/fields
+npm link ../plugins/dist/libs/common ../plugins/dist/libs/basic ../plugins/dist/libs/screen ../plugins/dist/libs/fields
