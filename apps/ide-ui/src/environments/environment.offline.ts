fbbb5f447c9520b8f7e1c254a888ebbe1acee397
Gérard Collin <gerard.collin@appliweb.net>
2023-09-15 16:54:42 +0200

    Stable & Next environments

--- apps/ide-ui/src/environments/environment.offline.ts
+++ apps/ide-ui/src/environments/environment.offline.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   webSocketUrl: 'wss://test.dont-code.net/ide?clientType=dev',
-  previewUrl: 'https://dont-code.net/preview-ui',
+  previewUrl: 'https://dont-code.net/preview-ui/next',
   projectUrl: '/api/project'
 };
