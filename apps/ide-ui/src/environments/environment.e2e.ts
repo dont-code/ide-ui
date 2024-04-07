37fb6f833cdb0e7c8e40dd2d2c8357a5c026d15a
Gérard Collin <gerard.collin@appliweb.net>
2024-04-07 12:45:06 +0200

    Urgent switch to new api urls

--- apps/ide-ui/src/environments/environment.e2e.ts
+++ apps/ide-ui/src/environments/environment.e2e.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  webSocketUrl: 'wss://test.dont-code.net/ide?clientType=test',
+  webSocketUrl: 'wss://test.dont-code.net/e2e/ide',
   previewUrl: 'https://dont-code.net/preview-ui/next',
-  projectUrl: 'https://test.dont-code.net/project'
+  projectUrl: 'https://test.dont-code.net/e2e/project'
 };
--- apps/ide-ui/src/environments/environment.yolo.ts
+++ apps/ide-ui/src/environments/environment.yolo.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'wss://yolo.test/dont-code/ide?clientType=demo',
+  webSocketUrl: 'wss://yolo.test/dc-demo/ide',
   previewUrl: 'http://localhost:5001',
-  projectUrl: 'https://yolo.test/dont-code/demo_project'
+  projectUrl: 'https://yolo.test/dc-demo/project'
 };
