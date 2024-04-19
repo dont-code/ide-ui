5f3f3f34c436f415c7c2681e57af36f59923e954
Gérard Collin <gerard.collin@appliweb.net>
2024-04-19 17:48:53 +0200

    Moved to run.dont-code.net urls

--- apps/ide-ui/src/environments/environment.e2e.ts
+++ apps/ide-ui/src/environments/environment.e2e.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  previewUrl: 'https://dont-code.net/preview-ui/next',
+  previewUrl: 'https://run.dont-code.net/preview-ui/next',
   //repositoryUrl: 'assets/repositories/next.json'
   repositoryUrl: 'https://test.dont-code.net/e2e/repository-next.json'
 };
--- apps/ide-ui/src/environments/environment.prod.ts
+++ apps/ide-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  previewUrl: 'https://dont-code.net/preview-ui/latest',
+  previewUrl: 'https://run.dont-code.net/preview-ui/latest',
   //repositoryUrl: 'assets/repositories/stable.json'
   repositoryUrl: 'https://test.dont-code.net/demo/repository-stable.json'
 };
