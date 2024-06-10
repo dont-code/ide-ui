951a08c60e44284855fdb36d8870d84085a757ae
Gérard Collin <gerard.collin@appliweb.net>
2024-06-10 20:04:05 +0200

    Text element using control

--- apps/ide-ui/src/app/routes/editor/input-element/input-element.component.ts
+++ apps/ide-ui/src/app/routes/editor/input-element/input-element.component.ts
@@ -1,36 +1,59 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { EditorElement } from '../editor-element';
 import { ChangeUpdateService } from '../../../shared/change/services/change-update.service';
 import { Change, ChangeType } from '@dontcode/core';
+import { FormControl } from '@angular/forms';
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'ide-ui-input-element',
   templateUrl: './input-element.component.html',
   styleUrls: ['./input-element.component.css']
 })
-export class InputElementComponent implements OnInit {
+export class InputElementComponent implements OnInit, OnDestroy {
   @Input()
   element!: EditorElement;
+
+  subscriptions = new Subscription ();
+  
+  control = new FormControl<string>('', {updateOn:'blur'});
 
   constructor(protected changeService:ChangeUpdateService) { }
 
   ngOnInit(): void {
+    this.updateView ();
+    this.subscriptions.add (
+      this.control.valueChanges.subscribe ({next: (value) => this.onChange(value)})
+    );
   }
 
-  onChange(event: Event) {
-    const target = event.target as HTMLInputElement;
+  ngOnDestroy(): void {
+      this.subscriptions.unsubscribe();
+  }
 
-    this.element.setEditedValue(target.value);
+  updateView (): void {
+    if (this.element?.getEditedValue()==null)
+      {
+        this.control.setValue('', {emitModelToViewChange:true, emitEvent:false});
+      } else {
+        this.control.setValue(this.element.getEditedValue(), {emitModelToViewChange:true, emitEvent:false});
+      }
+    
+    if ((this.element!=null) && (this.element.isReadonly()==true))
+        this.control.disable({emitEvent:false});
+      else
+        this.control.enable({emitEvent:false});
+    }
+
+  onChange(newValue: string|null): void {
+    if (this.element!=null) {
+    this.element.setEditedValue(newValue);
     this.changeService.pushChange(
       new Change(ChangeType.UPDATE,
         this.element.position,
-        target.value));
-  }
+        newValue));
 
-  editedValue() {
-    if (this.element.getEditedValue())
-      return this.element.getEditedValue();
-    else
-      return '';
-  }
+      }
+    }
+
 }
