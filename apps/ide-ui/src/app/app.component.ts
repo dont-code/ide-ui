b8bc385b6a06a350f09bb6e038ba0df7772fa782
Gérard Collin <gerard.collin@appliweb.net>
2024-06-16 10:54:11 +0200

    reset editor only at start

--- apps/ide-ui/src/app/app.component.ts
+++ apps/ide-ui/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, Inject, Injector } from '@angular/core';
-import { Core } from '@dontcode/core';
+import { Change, ChangeType, Core, DontCodeModel, dtcde } from '@dontcode/core';
 import { CommonConfigService, DONT_CODE_CORE } from '@dontcode/plugin-common';
 import { LightAppComponent } from '@dontcode/sandbox';
 import {environment} from '../environments/environment';
+import { ChangeUpdateService } from './shared/change/services/change-update.service';
+import { ProjectService } from './shared/project/services/project.service';
+import { TextService } from './shared/text/services/text.service';
+import { SandboxRepositorySchema } from '@dontcode/sandbox/lib/shared/definitions';
 
 @Component({
   selector: 'ide-ui-root',
@@ -13,7 +17,9 @@ import {environment} from '../environments/environment';
 export class AppComponent extends LightAppComponent{
   title = 'ide-ui';
 
-  constructor (configService:CommonConfigService, httpClient: HttpClient, injector: Injector, ref: ChangeDetectorRef,
+  constructor (configService:CommonConfigService, protected service:TextService
+    , protected updateService:ChangeUpdateService
+    , protected projectService: ProjectService, httpClient: HttpClient, injector: Injector, ref: ChangeDetectorRef,
     @Inject(DONT_CODE_CORE) dontCodeCore: Core
 ) {
     super (configService, httpClient, injector, ref, dontCodeCore);
@@ -29,4 +35,24 @@ export class AppComponent extends LightAppComponent{
     return window.location.hash.indexOf('/newTabDev')==-1;
   }
 
+  /**
+   * Initializa all services after all plugins and config are being loaded.
+   * @param config
+   * @param repoUrl 
+   */
+  protected afterInitialization(config: SandboxRepositorySchema, repoUrl: string): Promise<void> {
+      
+    this.service.resetSchema();
+    this.service.readSchema(dtcde.getSchemaManager().getSchema());
+  //    this.service.readSchemaFormUrl('assets/core/'+DontCode.dtcde.getSchemaUri());
+    return this.updateService.pushChange(new Change(ChangeType.RESET, DontCodeModel.ROOT,null)).then (
+      () => {
+        this.projectService.newCurrentProject();
+      },
+      (reason) => {
+        console.error ('Error while resetting model after a loadSchema', reason);
+        this.projectService.newCurrentProject();
+      }
+    )
+  }
 }
--- apps/ide-ui/src/app/layout/main/main.component.ts
+++ apps/ide-ui/src/app/layout/main/main.component.ts
@@ -56,7 +56,7 @@ export class MainComponent implements OnInit, OnDestroy{
         return {status:status[0], sessionId:status[1]};
       }));
 
-    this.loadSchema();
+//    this.loadSchema();
     }
 
   logoClicked() {
@@ -68,10 +68,12 @@ export class MainComponent implements OnInit, OnDestroy{
     this.sidePanelVisible=$event.target.visible;
   }
 
+  /**
+   * User wants to re-start from scratch
+   */
   loadSchema () {
     this.service.resetSchema();
     this.service.readSchema(dtcde.getSchemaManager().getSchema());
-//    this.service.readSchemaFormUrl('assets/core/'+DontCode.dtcde.getSchemaUri());
     this.updateService.pushChange(new Change(ChangeType.RESET, DontCodeModel.ROOT,null)).then (
       () => {
         this.projectService.newCurrentProject();
